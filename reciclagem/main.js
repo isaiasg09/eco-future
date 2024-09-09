ee8b666cd2d5a128781762d79b996824599abd41
isaiasg09 <isaiashsobral@gmail.com>
2024-09-09 18:50:23 -0300

    initiating reform of recycling page

---
+++ reciclagem/main.js
@@ -0,0 +1,9 @@
+function scrollar() {
+  let destination = document.querySelector(".lixeiras1");
+
+  // Scroll to the destination using
+  // scrollIntoView method
+  destination.scrollIntoView({
+    behavior: "smooth",
+  });
+}
